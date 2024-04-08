acfaeb199ac0d8ee3ceb98588d636ac9bd548d16
Anthony Duque <adduxe@hotmail.com>
2024-04-08 16:06:27 -0700

    Moved the repair order fetch from the Production controller to the Estimator Controller.

--- scripts/Estimator_View.js
+++ scripts/Estimator_View.js
@@ -1,7 +1,36 @@
 
 var EstimatorViewCtrlr =
+
     function($scope, $http){
         $scope.sample = "Estimators";
+
+        GetRepairOrders();
+
+        function handleSuccess(response)
+        {
+            if (response.data){
+             console.log("Repair records fetched successfully!");
+             console.log(response.data);
+             $scope.repairOrders = response.data;
+            }
+        }
+
+        function handleError(response)
+        {
+            console.log("Repair records not fetched.");
+            //console.log(response.status);
+            //console.log(response.statusText);
+            //console.log(response.headers());
+        }
+
+        function GetRepairOrders()
+        {
+
+            $http.get('./php/Production.php')
+                  .then(handleSuccess)
+                  .catch(handleError);   // .then()
+        }
+
     };
 
 app.controller("EstimatorViewController", EstimatorViewCtrlr);
--- scripts/Production.js
+++ scripts/Production.js
@@ -5,33 +5,6 @@ var app = angular
 
         $scope.productionView = 'Estimator_View.html';
 
-        GetRepairOrders();
-
-        function handleSuccess(response)
-        {
-            if (response.data){
-             console.log("Repair records fetched successfully!");
-             console.log(response.data);
-             $scope.repairOrders = response.data;
-            }
-        }
-
-        function handleError(response)
-        {
-            console.log("Repair records not fetched.");
-            //console.log(response.status);
-            //console.log(response.statusText);
-            //console.log(response.headers());
-        }
-
-        function GetRepairOrders()
-        {
-
-            $http.get('./php/Production.php')
-                  .then(handleSuccess)
-                  .catch(handleError);   // .then()
-        }
-
         $scope.chooseView = function(name){
 
             switch(name){
