6f1b7b9f0882ab6513ac1ea358cdc2ce380fb97f
Anthony Duque <adduxe@hotmail.com>
2024-04-09 21:32:32 -0700

    Working code displaying cars per Estimators or Technicians with just 2 buttons at the top of the main page.

--- scripts/Production.js
+++ scripts/Production.js
@@ -3,32 +3,6 @@ var app = angular
   .module("prodModule", [])
     .controller("prodController", function($scope, $http){
 
-        $scope.productionView = 'Estimator_View.html';
-
-        $scope.chooseView = function(name){
-
-            switch(name){
-
-                case 'Sep':
-                case 'Jim':
-                case 'Tony':
-                case 'Anthony':
-                case 'Chad':
-                    $scope.productionView = 'Estimator_View.html';
-                    break;
-
-                case 'Van':
-                case 'Jose':
-                case 'Jerry':
-                case 'Nacho':
-                case 'Brian':
-                    $scope.productionView = 'Technician_View.html';
-                    break;
-
-                default:
-                    $scope.productionView = 'All_View.html';
-                    break;
-            }
-        }
+        $scope.productionView = 'Technician_View.html';
 
     });
