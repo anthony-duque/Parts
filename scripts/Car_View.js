0fb6244ecc720242b35c437f1626709b872d9f3c
Anthony Duque <adduxe@hotmail.com>
2024-04-12 13:53:48 -0700

    Car view page succesfully getting ro num from query string.

--- scripts/Car_View.js
+++ scripts/Car_View.js
@@ -2,7 +2,10 @@ var app = angular.module("carViewModule", []);
 
 var carViewCtrlr = function($scope, $http){
 
-    $scope.sample = "sample";
+    var params = getQueryParams(window.location.href);
+
+    $scope.sample = params.roNum;
+
 
     $scope.car = {
         "ro_num"    : 2594,
@@ -12,6 +15,18 @@ var carViewCtrlr = function($scope, $http){
         "estimator" : "Sep Sadhegi"
     };
 
+    function getQueryParams(url) {
+
+      const queryString = url.split('?')[1];
+      const params = {};
+      if (queryString) {
+        queryString.split('&').forEach((param) => {
+          const [key, value] = param.split('=');
+          params[key] = decodeURIComponent(value);
+        });
+      }
+      return params;
+    }
 }
 
 app.controller("carViewController", carViewCtrlr);
