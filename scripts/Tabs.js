a9ec03ee93f77db4d635db47339c44a4bfa4d8d9
Anthony Duque <adduxe@hotmail.com>
2024-05-09 22:51:18 -0700

    Deleted sample code files (New*.html, Received.html, etc.)

--- scripts/Tabs.js
+++ scripts/Tabs.js
@@ -3,13 +3,33 @@ var app = angular.module("TabsApp", []);
 var TabsCtrlr = function($scope){
 
     $scope.Tabs =
-        { 'Production':"active", 'PartSearch':"inactive", 'PaintList': "inactive", 'Upload':"inactive" };
+        { 'Production':"active", 'PartsSearch':"inactive", 'PaintList': "inactive", 'Upload':"inactive" };
 
     $scope.PickTab = function(tabName){
+
         for (var key in $scope.Tabs){
             $scope.Tabs[key] = 'inactive';
         }
-        $scope.Tabs[tabName] = 'active'
+
+        $scope.Tabs[tabName] = 'active';
+
+        var tabView;
+        switch(tabName){
+
+            case 'Production':
+                tabView = 'Production.html';
+                break;
+
+            case 'PartsSearch':
+                tabView = 'Parts_Search.html';
+                break;
+
+            default:
+                tabView = 'index.html';
+                break;
+        }
+
+        $scope.tabView = tabView;
     }   // PickTab()
 
 }
