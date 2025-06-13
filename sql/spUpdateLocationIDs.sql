c8b5b7596d8017f7dce279bb3669acf4c2ce9a3c
Anthony Duque <adduxe@hotmail.com>
2025-06-12 18:57:15 -0700

    Stored Procedure spUpdateLocationIDs: Takes in all cars that are checked in except completed ones.

--- sql/spUpdateLocationIDs.sql
+++ sql/spUpdateLocationIDs.sql
@@ -1,4 +1,4 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `CarStar`.`spUpdateLocationIDs`()
+CREATE PROCEDURE spUpdateLocationIDs()
 BEGIN
 
 	INSERT INTO Location_IDs
@@ -26,18 +26,17 @@ BEGIN
 
 	INSERT INTO Car_Stage
 		(ro_Num, loc_ID, stage_ID)
-	SELECT r.RONum, r.Loc_ID, FLOOR(SUBSTRING_INDEX(r.CurrentPhase, " ", 1)) AS stageID
+	SELECT r.RONum, r.Loc_ID,
+		CASE
+			WHEN SUBSTRING_INDEX(r.CurrentPhase, " ", 1) REGEXP '[0-9]'
+			THEN FLOOR(SUBSTRING_INDEX(r.CurrentPhase, " ", 1))
+			ELSE 0
+		END AS stageID 
 	FROM Repairs r LEFT JOIN Car_Stage ps
 		ON r.RONum = ps.ro_Num AND r.Loc_ID = ps.loc_ID
-	WHERE ps.id IS NULL AND r.CurrentPhase NOT LIKE '[%%]'
+	WHERE ps.id IS NULL
+			AND r.CurrentPhase <> '[Completed]'
+			AND Vehicle_In < DATE_ADD(CURDATE(), INTERVAL 1 DAY)
 	ORDER BY r.RONum;
-
-	DELETE FROM Tech_Car_Priority
-	WHERE id IN
-		(SELECT * FROM (SELECT tcp.id
-						FROM Tech_Car_Priority tcp LEFT JOIN Repairs r
-							ON tcp.RO_Num = r.RONum AND tcp.LocationID = r.Loc_ID
-						WHERE r.id IS NULL) AS p
-		);
 
 END
