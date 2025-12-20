199e7ec7f073ce581681d370c85bf9bddd6e54fe
Anthony Duque <adduxe@hotmail.com>
2025-12-19 21:35:34 -0800

    HTML formatting changes

--- scripts/Login.js
+++ scripts/Login.js
@@ -3,11 +3,18 @@ var app = angular.module('LoginApp', []);
 function loginController($scope, $http) {
 
     $scope.x = 10;
-  $scope.login = function() {
-    // Perform login logic here
-    // For example, validate user credentials and redirect to the dashboard if successful
-    // You can use AngularJS services or HTTP requests to handle the login process
-  };
+    $scope.loginCorrect = true;
+
+    $scope.login = function(username, password) {
+            // Perform login logic here
+            // For example, validate user credentials and redirect to the dashboard if successful
+            // You can use AngularJS services or HTTP requests to handle the login process            
+        if (username === 'admin' && password === 'password') {
+            $scope.loginCorrect = true;
+        } else {
+            $scope.loginCorrect = false;
+        }
+    };
 }
 
 app.controller('LoginAppController', loginController);
