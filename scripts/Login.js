7218d70f274996bb1792635f0817a46d852f51c6
Sonny <adduxe@hotmail.com>
2026-05-30 22:06:09 -0700

    Login.js - stores the location IDs in a session variable.  Redirects to the Admin page to upload first extracts if no shops are found for the company yet.

--- scripts/Login.js
+++ scripts/Login.js
@@ -1,7 +1,7 @@
 
 var app = angular.module('LoginApp',[]);
 
-function loginController($scope, $http) {
+function loginController($scope, $http, $window) {
 
     $scope.loginError = '';
 
@@ -12,16 +12,35 @@ function loginController($scope, $http) {
 
                 $scope.loginResult = response.data;
 
-                if ($scope.loginResult == 'true'){
+                if ($scope.loginResult.success === true){   // company has an account
 
-                    window.location.href = '../index.html';
+                    if ($scope.loginResult.locationIDs) {   // company has shops associated with the account
 
-                } else {
-                    $scope.loginError = $scope.loginResult;
+                            // store locationIDs in session storage for use in other pages
+                        $window.sessionStorage.setItem('locationIDs', 
+                                JSON.stringify($scope.loginResult.locationIDs));
+                        
+                        $window.location.href = '../index.html';
+
+                    } else {    // company has no shops associated with the account
+
+                        alert("Login successful, but no shops are associated with this account. Please upload CSV extracts.");
+                        $window.location.href = './admin/Admin.html';
+
+                    }
+
+                } else {    // login failed ($scope.loginResult.success === false)
+
+                    $scope.loginError = $scope.loginResult.message;
                 }
             });
     };  // $scope.login()
 
 }   // loginController()
 
-app.controller('LoginAppController', loginController);
+app.controller(
+    'LoginAppController', 
+    [ 
+        '$scope', '$http', '$window', loginController
+    ]
+);
