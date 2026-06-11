e4895830ecdd18066730072c6092ad5e7318ac45
Sonny <adduxe@hotmail.com>
2026-06-11 00:53:57 -0700

    Upload Extract split into 2 files (untested).

---
+++ php/admin/Process_Extract_File.php
@@ -0,0 +1,169 @@
+<?php
+
+const SHOP_ID       = 0;
+const SHOP_NAME     = 1;
+const RO_NUM        = 2;
+const ESTIMATOR     = 3;
+const TECHNICIAN    = 4;
+const OWNER         = 5;
+const VEHICLE_COLOR = 6;
+const LICENSE_PLATE = 7;
+const VIN           = 8;
+const VEHICLE_YEAR  = 9;
+const VEHICLE_MAKE  = 10;
+const VEHICLE_MODEL = 11;
+const CURRENT_PHASE = 12;
+const DATE_IN       = 13;
+const TARGET_DATE   = 14;
+const LINE_NO       = 15;
+const OP_CODE       = 16;
+const PART_DESC     = 17;
+const PART_NO       = 18;
+const PART_TYPE     = 19;
+const PART_STATUS   = 20;
+const VENDOR_NAME   = 21;
+const PART_PRICE    = 22;
+const RO_QTY        = 23;
+const ORDER_DATE    = 24;
+const ORDERED_QTY   = 25;
+const RECEIVED_QTY  = 26;
+const RECEIVED_DATE = 27;
+const RETURNED_QTY  = 28;
+
+require('../Utility_Scripts.php');
+
+function Process_Extract($extract_file, $companyID){
+
+    // Open the extract file for reading
+    if (($handle = fopen($extract_file, "r")) === FALSE) {
+        echo "Error in opening " . $extract_file;
+        exit;
+    }
+
+    $insert_sql = <<<strSQL
+            INSERT INTO extract_file_dump
+                (shop_id, shop_name, ro_num, estimator, technician,
+                owner, vehicle_color, license_plate, vin, vehicle_year,
+                vehicle_make, vehicle_model, current_phase, date_in,
+                target_date, line_num, repair_code, part_desc, 
+                part_num, part_type, part_status, vendor_name, part_price,
+                ro_qty, order_date, order_qty, received_qty, received_date, 
+                return_qty, company_id)
+            VALUES
+strSQL;
+
+    $row    = 0;  // record counter
+    $values = '';
+
+    while (($data = fgetcsv($handle, 500, ",")) !== FALSE){
+
+        if ($row == 0){
+
+            ++$row;
+            continue;   // skip header row
+
+        } else {
+
+            $shop_id        = "'" . $data[SHOP_ID] . "'";
+
+            $shop_name      = "'" . $data[SHOP_NAME] . "'";
+
+            $ro_num         = "'" . $data[RO_NUM] . "'";
+
+            $estimator      = "'" . Cleanup_Text($data[ESTIMATOR]) . "'";
+
+            $technician     = "'" . Cleanup_Text($data[TECHNICIAN]) . "'";
+
+            $owner          = "'" . Cleanup_Text($data[OWNER]) . "'";
+
+            $vehicle_color  = "'" . $data[VEHICLE_COLOR] . "'";
+
+            $license_plate  = "'" . $data[LICENSE_PLATE] . "'";
+
+            $vin            = "'" . $data[VIN] . "'";
+
+            $vehicle_year   = $data[VEHICLE_YEAR];
+
+            $vehicle_make   = "'" . $data[VEHICLE_MAKE] . "'";
+
+            $vehicle_model  = "'" . $data[VEHICLE_MODEL] . "'";
+
+            $current_phase  = "'" . $data[CURRENT_PHASE] . "'";
+
+            $date_in        = Get_SQL_date($data[DATE_IN]);
+
+            $target_date    = Get_SQL_date($data[TARGET_DATE]);
+
+            $line_no        = $data[LINE_NO];
+
+            $op_code        = "'" . $data[OP_CODE] . "'";
+
+            $part_desc      = "'" . Cleanup_Text($data[PART_DESC]) . "'";
+
+            $part_no        = "'" . Cleanup_Text($data[PART_NO]) . "'";
+
+            $part_type      = "'" . $data[PART_TYPE]. "'";
+
+            $part_status    = $data[PART_STATUS];
+
+            $vendor_name    = "'" . Cleanup_Text($data[VENDOR_NAME]) . "'";
+
+            $part_price     = Get_Quantity(str_replace("$", "", $data[PART_PRICE]));
+
+            $ro_qty         = Get_Quantity($data[RO_QTY]);
+
+            $order_date     = Get_SQL_date($data[ORDER_DATE]);
+
+            $ordered_qty    = Get_Quantity($data[ORDERED_QTY]);
+
+            $received_qty   = Get_Quantity($data[RECEIVED_QTY]);
+
+            $received_date  = Get_SQL_date($data[RECEIVED_DATE]);
+
+            $returned_qty   = Get_Quantity($data[RETURNED_QTY]);
+
+            $values .= "(" . $shop_id . ", " . $shop_name . ", " . $ro_num . ", " .
+                        $estimator . ", " . $technician . ", " . $owner . ", " .
+                        $vehicle_color . ", " . $license_plate . ", " . $vin . ", " .
+                        $vehicle_year . ", " . $vehicle_make . ", " . $vehicle_model . ", " .
+                        $current_phase . ", " . $date_in . ", " . $target_date . ", " .
+                        $line_no . ", " . $op_code. ", " . $part_desc. ", " .
+                        $part_no. ", " . $part_type. ", '" .$part_status. "', ".
+                        $vendor_name. ", ". $part_price. ", ". $ro_qty. ", ".
+                        $order_date. ", ". $ordered_qty. ", ". $received_qty. ", ".
+                        $received_date. ", ". $returned_qty . "," .
+                        $companyID . "),";
+  
+            ++$row;
+
+        }   // if ($row == 0)
+
+    }   // while (($data = fgetcsv($handle, 500, ",")) !== FALSE)
+
+    $values = rtrim($values, ",");   // remove trailing comma
+    $insert_sql .= $values;
+
+    echo $insert_sql . '<br/><br/>';
+
+    require('../db_open.php');
+
+        // Delete all records from the Extract table for the company ID.
+    $tsql = "DELETE FROM extract_file_dump " . 
+            "WHERE companyID = $companyID";
+
+    if ($conn->query($tsql) === TRUE) {
+        echo "<br/><br/>Extract records for $companyID deleted.<br/>";
+    } else {
+      echo "Error: " . $tsql . "<br> - " . $conn->error;
+      exit;
+    }
+
+        // Insert the new records from the extract file.
+    if ($conn->query($insert_sql) === TRUE) {
+        echo "<br/><br/>Extract records for $companyID inserted successfully.<br/>";
+    } else {
+      echo "Error: " . $insert_sql . "<br> - " . $conn->error;
+      exit;
+    }
+}
+?>
--- php/admin/Upload_Extract.php
+++ php/admin/Upload_Extract.php
@@ -4,38 +4,10 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+require('Process_Extract_File.php');
+
 const TARGET_DIR        = "../../extract_files/";   // destination folder on the server
 const EXTRACT_FILENAME  = "Extract_CompanyID.csv";  // Daily Out destination file name
-
-const SHOP_ID       = 0;
-const SHOP_NAME     = 1;
-const RO_NUM        = 2;
-const ESTIMATOR     = 3;
-const TECHNICIAN    = 4;
-const OWNER         = 5;
-const VEHICLE_COLOR = 6;
-const LICENSE_PLATE = 7;
-const VIN           = 8;
-const VEHICLE_YEAR  = 9;
-const VEHICLE_MAKE  = 10;
-const VEHICLE_MODEL = 11;
-const CURRENT_PHASE = 12;
-const DATE_IN       = 13;
-const TARGET_DATE   = 14;
-const LINE_NO       = 15;
-const OP_CODE       = 16;
-const PART_DESC     = 17;
-const PART_NO       = 18;
-const PART_TYPE     = 19;
-const PART_STATUS   = 20;
-const VENDOR_NAME   = 21;
-const PART_PRICE    = 22;
-const RO_QTY        = 23;
-const ORDER_DATE    = 24;
-const ORDERED_QTY   = 25;
-const RECEIVED_QTY  = 26;
-const RECEIVED_DATE = 27;
-const RETURNED_QTY  = 28;
 
 $companyID = $_GET["companyID"];    // get the company ID from the query string
 
@@ -57,28 +29,4 @@ try{
     header("Location: ../../html/admin/Upload_Extract.html");
 
 }
-
-
-function Process_Extract($extract_file, $companyID){
-
-    // Open the extract file for reading
-    if (($handle = fopen($extract_file, "r")) === FALSE) {
-        echo "Error in opening " . $extract_file;
-        exit;
-    }
-
-
-
-    require('../db_open.php');
-
-    $tsql = "CALL sp_Process_Extract('$companyID')";
-    if ($conn->query($tsql) === TRUE) {
-        echo "<br/><br/>Extract processed successfully.<br/>";
-    } else {
-      echo "Error: " . $tsql . "<br> - " . $conn->error;
-      exit;
-    }
-
-}
-
 ?>
